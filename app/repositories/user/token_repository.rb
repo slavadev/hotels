7d0a09aaed5fe598a94a8ca43744a4a627b9a47a
korolvs <korolvs@gmail.com>
2016-09-19 12:23:59 +0300

    User and Token repositories

---
+++ app/repositories/user/token_repository.rb
@@ -0,0 +1,16 @@
+# Contains methods to work with token entities
+class User::TokenRepository < Core::Repository
+  # Sets all variables
+  # @see User::Token
+  def initialize
+    @model = User::Token
+  end
+
+  # Finds token by code and type
+  # @param [Integer] code
+  # @param [Integer] type
+  # @return [User::Token]
+  def find_token_by_code_and_type(code, type)
+    @model.where(code: code, token_type: type).first
+  end
+end
